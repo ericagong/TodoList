d0396d22557e5de41a50b7595ecf47c36b165627
ericagong <48196721+ericagong@users.noreply.github.com>
2022-07-23 09:34:33 +0900

    clean Bucket

--- src/App.js
+++ src/App.js
@@ -1,93 +1,34 @@
 import React from "react";
 import styled from "styled-components";
-import Nemo from "./Nemo";
-// import BucketList from "./BucketList";
-// import "./style.css";
+import BucketList from "./BucketList";
+import "./style.css";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+const App = (props) => {
+  const [list, setList] = React.useState([
+    "JS 파트 2 정리하기",
+    "3주차까지 듣기",
+    "자전 전화하기",
+  ]);
+  const new_todo = React.useRef(null);
 
-    this.state = { count: 3 };
-  }
-
-  addNemo = () => {
-    if (this.state.count < 10) {
-      this.setState({ count: this.state.count + 1 });
-    } else {
-      window.alert("네모가 너무 많아요. 기존 네모를 줄여야 더할수 있답니다 :(");
-    }
+  const addBucket = () => {
+    const new_item = new_todo.current.value;
+    setList([...list, new_item]);
+    new_todo.current.value = "";
   };
 
-  removeNemo = () => {
-    if (this.state.count > 0) {
-      this.setState({ count: this.state.count - 1 });
-    } else {
-      window.alert("네모가 없어요 :(");
-    }
-  };
-
-  render() {
-    // console.log(this.state.count);
-    // const nemo_cnts = Array.from({ length: this.state.count }, (_, i) => i);
-    return (
-      <div className='App'>
-        <h1>Nemo Project</h1>
-        <Nemo></Nemo>
-        {/* {nemo_cnts.map((_, i) => (
-          <div
-            className='Nemo'
-            key={i}
-            style={{
-              width: "100px",
-              height: "100px",
-              backgroundColor: "gray",
-              margin: "10px",
-            }}
-          ></div>
-        ))}
-        <button onClick={this.addNemo}>네모 추가하기</button>
-        <button
-          onClick={() => {
-            this.removeNemo();
-          }}
-        >
-          네모 없애기
-        </button> */}
-      </div>
-    );
-  }
-}
-
-// class App extends React.Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {
-//       list: ["JS 파트 2 정리하기", "3주차까지 듣기", "자전 전화하기"],
-//     };
-
-//     // this.text = React.createRef();
-//   }
-
-//   // componentDidMount() {
-//   //   console.log(this.text.current);
-//   // }
-
-//   render() {
-//     return (
-//       <Wrapper>
-//         <Container>
-//           <BucketList list={this.state.list} />
-//           {/* <div>
-//             <input type='text' ref={this.text} />
-//             <button></button>
-//           </div> */}
-//         </Container>
-//       </Wrapper>
-//     );
-//   }
-// }
+  return (
+    <Wrapper>
+      <Container>
+        <BucketList list={list} />
+        <div>
+          <Input ref={new_todo}></Input>
+          <Button onClick={addBucket}>Submit</Button>
+        </div>
+      </Container>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.div`
   background-color: #eee;
@@ -108,12 +49,18 @@ const Container = styled.div`
   text-align: center;
 `;
 
-// function App() {
-//   return (
-//     <div className='App'>
-//       <BucketList />
-//     </div>
-//   );
-// }
+const Input = styled.input`
+  margin: auto;
+  height: 30px;
+  width: 75%;
+  border: 1px solid #ddd;
+`;
+
+const Button = styled.button`
+  padding: 8px;
+  background-color: slateblue;
+  color: white;
+  border: 1px solid #ddd;
+`;
 
 export default App;
--- src/BucketList.js
+++ src/BucketList.js
@@ -1,22 +1,6 @@
-/* 클래스형 컴포넌트 사용 */
 import React from "react";
 import styled from "styled-components";
-// class BucketList extends React.Component {
-//   constructor(props) {
-//     super(props);
 
-//     this.state = {
-//       list: [],
-//     };
-//   }
-
-//   // render는 무조건 있어야 함.
-//   render() {
-//     return <div>버킷리스트</div>;
-//   }
-// }
-
-/* 함수형 컴포넌트 사용 */
 const BucketList = ({ list }) => {
   const todos = list.map((todo, i) => <Item key={i}>{todo}</Item>);
   const wrapper = React.useRef(null); // 리액트 훅 사용, 초기값 제공
@@ -24,7 +8,6 @@ const BucketList = ({ list }) => {
   console.log(wrapper);
 
   window.setTimeout(() => {
-    // 1초 뒤에는?!
     console.log(wrapper);
   }, 1000);
   return (
@@ -51,11 +34,4 @@ const Item = styled.div`
   background-color: aliceblue;
 `;
 
-// function BucketList(props) {
-// 	return (
-// 		<div>버킷리스트</div>
-// 	)
-// }
-
 export default BucketList;
-// export { BucketList };
