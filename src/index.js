ea7d34c176381d151111e276500ea491dc5c4cd7
ericagong <48196721+ericagong@users.noreply.github.com>
2022-07-26 21:52:27 +0900

    superConverter

--- src/index.js
+++ src/index.js
@@ -7,14 +7,59 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/configStore";
 
+const Wrap = (props) => {
+  const [amount, setAmount] = React.useState(0);
+  const [disabled, setDisabled] = React.useState(true);
+
+  const changeHandler = (e) => {
+    setAmount(e.target.value);
+  };
+
+  const resetHandler = () => setAmount(0);
+  const flipHandler = () => {
+    setDisabled((curr) => !curr);
+    setAmount(0);
+  };
+  const Conatiner = () => (
+    <div>
+      <h1>Super Converter</h1>
+      <label htmlFor='minutes'>Minutes</label>
+      <input
+        type='number'
+        id='minutes'
+        placeholder='minutes'
+        value={!disabled ? amount * 60 : amount}
+        onChange={changeHandler}
+        disabled={!disabled}
+      />
+      <h3>You want to convert {amount}</h3>
+      <label htmlFor='hours'>Hours</label>
+      <input
+        type='number'
+        id='hours'
+        placeholder='hours'
+        value={disabled ? amount / 60 : amount}
+        onChange={changeHandler}
+        disabled={disabled}
+      />
+      <button onClick={resetHandler}>reset</button>
+      <button onClick={flipHandler}>flip</button>
+    </div>
+  );
+
+  return <Conatiner></Conatiner>;
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
-);
+root.render(<Wrap />);
+
+// root.render(
+//   <Provider store={store}>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </Provider>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
